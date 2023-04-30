e29cf2bb3c110b6545a54e4a428bdfba03422ecc
Kerollos Boles <boleskoko9@gmail.com>
2023-04-30 14:32:38 +0300

    makes the app able to be good with any widget

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final double widthScreen = MediaQuery.of(context).size.width;
+    return Scaffold();
   }
 }
