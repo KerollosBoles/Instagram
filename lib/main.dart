93a941c008e8de074de7a8271c06df5d283ba251
Kerollos Boles <boleskoko9@gmail.com>
2023-04-30 14:23:11 +0300

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -3,4 +3,15 @@ import 'package:flutter/src/widgets/placeholder.dart';
 
 import 'package:flutter/src/widgets/framework.dart';
 
-import 'package/flutter/material.dart';
+import 'package/flutte.//,iir/material.dart';
+
+void main() {}
+
+class main extends StatelessWidget {
+  const main({Key : key}) : super(key?key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
