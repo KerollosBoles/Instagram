0cdde49f97454360956c3b60c9695d7962050e43
Kerollos Boles <boleskoko9@gmail.com>
2023-04-30 15:47:47 +0300

    edit web screen widget

--- lib/responsive/web.dart
+++ lib/responsive/web.dart
@@ -1,1 +1,14 @@
+import 'package:flutter/material.dart';
 
+class Web extends StatelessWidget {
+  const Web({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Web Screen"),
+      ),
+    );
+  }
+}
