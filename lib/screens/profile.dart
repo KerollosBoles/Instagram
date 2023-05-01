dbb234b7f59113b91792d959fec705302b93dd07
Kerollos Boles <boleskoko9@gmail.com>
2023-05-01 13:43:40 +0300

    importing full Search

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class ProfilesScreen extends StatelessWidget {
-  const ProfileScreen({Key? key}) : super(key: key);
+  const ProfilesScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
