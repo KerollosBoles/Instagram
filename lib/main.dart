89ed1daadbe7d956844cd208cc1e087b78f7eb6e
Kerollos Boles <boleskoko9@gmail.com>
2023-05-01 13:13:54 +0300

    make the theme become dark

--- lib/main.dart
+++ lib/main.dart
@@ -16,8 +16,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
       home:
           Responsive(myMobileScreen: MobileScreen(), myWebScreen: WebScreen()),
     );
