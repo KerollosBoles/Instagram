1c874ecdf87f1946c3ee3866856416d4d9b8348f
Kerollos Boles <boleskoko9@gmail.com>
2023-05-01 13:42:52 +0300

    importing full profile

---
+++ lib/screens/profile.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ProfilesScreen extends StatelessWidget {
+  const ProfileScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
+//importing full add_posts 
