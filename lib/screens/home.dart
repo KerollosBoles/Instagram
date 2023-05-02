01136b578877261b495006c502bb48e68ff4965d
Kerollos Boles <boleskoko9@gmail.com>
2023-05-02 12:19:55 +0300

    importing body

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -13,23 +13,27 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: mobileBackgroundColor,
-      appBar: AppBar(
-        actions: [
-          IconButton(
-              onPressed: () {},
-              icon: Icon(
-                Icons.messenger_outline,
-              ))
-        ],
         backgroundColor: mobileBackgroundColor,
-        title: SvgPicture.asset(
-          "assets/img/instagram.svg",
-          color: primaryColor,
-          height: 35,
+        appBar: AppBar(
+          actions: [
+            IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.messenger_outline,
+                ))
+          ],
+          backgroundColor: mobileBackgroundColor,
+          title: SvgPicture.asset(
+            "assets/img/instagram.svg",
+            color: primaryColor,
+            height: 35,
+          ),
         ),
-      ),
-    );
+        body: Column(
+          children: [
+            Row(),
+          ],
+        ));
   }
 }
 //1:24
