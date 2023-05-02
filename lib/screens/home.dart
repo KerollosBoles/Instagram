143b899f159965106e04fc648b20e4ca3ca6389c
Kerollos Boles <boleskoko9@gmail.com>
2023-05-02 12:09:45 +0300

    makes the home app bar become black

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -14,6 +14,7 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: mobileBackgroundColor,
         title:
             SvgPicture.asset("assets/img/instagram.svg", color: primaryColor),
       ),
