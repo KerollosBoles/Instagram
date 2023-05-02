01644509ec8ee590fd98299aa0699d5783a23189
Kerollos Boles <boleskoko9@gmail.com>
2023-05-02 12:30:34 +0300

    put the user name and its font size

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -41,6 +41,10 @@ class _HomeState extends State<Home> {
                 ),
                 SizedBox(
                   width: 17,
+                ),
+                Text(
+                  "Kerollos Boles",
+                  style: TextStyle(fontSize: 18),
                 )
               ],
             ),
