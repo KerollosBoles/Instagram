1fc3dfe225c7f2134bf53dc38716cb8c95522aad
Kerollos Boles <boleskoko9@gmail.com>
2023-05-02 12:10:32 +0300

    makes the the scaffold background color is black

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -13,6 +13,7 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: mobileBackgroundColor,
       appBar: AppBar(
         backgroundColor: mobileBackgroundColor,
         title:
