580beffab319b12a773303b92e15d19c90d48398
Kerollos Boles <boleskoko9@gmail.com>
2023-05-02 13:58:30 +0300

    finishing web widget

--- lib/responsive/web.dart
+++ lib/responsive/web.dart
@@ -1,4 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../screens/add_post.dart';
+import '../screens/home.dart';
+import '../screens/profile.dart';
+import '../screens/search.dart';
+import '../shared/colors.dart';
 
 class WebScreen extends StatefulWidget {
   const WebScreen({super.key});
@@ -11,8 +18,63 @@ class _WebScreenState extends State<WebScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-      title: Text("Web Screen"),
-    ));
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            icon: Icon(
+              Icons.home,
+              color: primaryColor,
+            ),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(
+              Icons.search,
+              color: secondaryColor,
+            ),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(
+              Icons.add_a_photo,
+              color: secondaryColor,
+            ),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(
+              Icons.favorite,
+              color: secondaryColor,
+            ),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(
+              Icons.person,
+              color: secondaryColor,
+            ),
+            onPressed: () {},
+          ),
+        ],
+        backgroundColor: mobileBackgroundColor,
+        title: SvgPicture.asset(
+          "assets/img/instagram.svg",
+          color: primaryColor,
+          height: 32,
+        ),
+      ),
+      body: PageView(
+        onPageChanged: (index) {},
+        physics: NeverScrollableScrollPhysics(),
+        // controller: _pageController,
+        children: [
+          Home(),
+          Search(),
+          AddPost(),
+          Center(child: Text("Love u ♥")),
+          Profile(),
+        ],
+      ),
+    );
   }
 }
