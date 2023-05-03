6f789c5d2979c80d3d98061221d4af47893ecb63
Kerollos Boles <boleskoko9@gmail.com>
2023-05-03 09:57:20 +0300

    edit web screen

---
+++ lib/screens/register.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../screens/add_post.dart';
+import '../screens/home.dart';
+import '../screens/profile.dart';
+import '../screens/search.dart';
+import '../shared/colors.dart';
--- lib/screens/search.dart
+++ lib/screens/search.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
+import '../screens/add_post.dart';
+import '../screens/home.dart';
+import '../screens/profile.dart';
+import '../screens/search.dart';
+import '../shared/colors.dart';
 import '../shared/colors.dart';
 
 class Search extends StatefulWidget {
