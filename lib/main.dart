bdedfea5f3e345196fac7234ca529c634277d068
Kerollos Boles <boleskoko9@gmail.com>
2023-05-03 09:54:57 +0300

    edit main to let login start first

--- lib/main.dart
+++ lib/main.dart
@@ -14,13 +14,15 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark(),
-      home:
-          Responsive(myMobileScreen: MobileScreen(), myWebScreen: WebScreen()),
+      home: Login(),
+      // home: Resposive(
+      //   myMobileScreen: MobileScerren(),
+      //   myWebScreen: WebScerren(),
+      // ),
     );
   }
 }
